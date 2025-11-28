45a64457c41a342fa0e1fb1153010b1a7d5da61d
gustavoalvim41 <gustavoalvim85@gmail.com>
2025-11-28 15:10:52 -0300

    update ingest controller

--- backend/src/controllers/ingest.controller.ts
+++ backend/src/controllers/ingest.controller.ts
@@ -7,7 +7,10 @@ export default class IngestController {
       if (!req.file) {
         return res.status(400).json({ error: 'File not sent' });
       }
-
+      const allowedMimeTypes = ['text/plain', 'application/pdf'];
+      if (!allowedMimeTypes.includes(req.file.mimetype)) {
+        return res.status(400).json({ error: 'Only .txt or .pdf files are allowed' });
+      }
       const result = await IngestService.process(req.file);
       res.json(result);
     } catch (err: any) {
