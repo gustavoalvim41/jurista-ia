1493672229704f4510e707b188d8bf50823e11d4
gustavoalvim41 <gustavoalvim85@gmail.com>
2025-11-28 18:27:18 -0300

    update ext .txt .pdf

--- backend/src/services/rag.service.ts
+++ backend/src/services/rag.service.ts
@@ -1,15 +1,24 @@
+import { PDFParse } from 'pdf-parse';
+
 export default class RagService {
   static async extractText(file: Express.Multer.File): Promise<string> {
-    const ext = file.originalname.split(".").pop()?.toLowerCase();
+    const ext = file.originalname.split('.').pop()?.toLowerCase();
 
-    if (ext === "txt") {
-      return file.buffer.toString("utf-8");
+    if (ext === 'txt') {
+      return file.buffer.toString('utf-8');
     }
 
-    if (ext === "pdf") {
+    if (ext === 'pdf') {
+      const parser = new PDFParse({ data: file.buffer });
 
+      try {
+        const result = await parser.getText();
+        return result.text;
+      } finally {
+        await parser.destroy();
+      }
     }
 
-    throw new Error("File type not supported");
+    throw new Error('File type not supported');
   }
 }
