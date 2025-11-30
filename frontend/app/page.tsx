10ab2dd5c0247c3e912f0fe3dccafc55871e00a6
gustavoalvim41 <gustavoalvim85@gmail.com>
2025-11-30 19:08:45 -0300

    update chat page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,37 +1,52 @@
 "use client";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Container from "@/components/container";
 import { Plus, MoveUp } from "lucide-react";
 
-// import Image from "next/image";
-// import pdf from "../public/pdf.svg";
-
 export default function Home() {
-  const [file, setFile] = useState<boolean>(false);
+  const [file, setFile] = useState<File | null>();
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    inputRef.current?.click();
+  };
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0] ?? null;
+    if (selectedFile) {
+      setFile(selectedFile);
+      console.log("Arquivo selecionado:", selectedFile.name);
+    }
+  };
+
   return (
     <Container>
       <div className="h-screen flex flex-col items-center justify-center gap-4">
         <div className="min-w-full">
-          <h1 className="text-2xl font-medium tracking-normal leading-relaxed text-foreground/90 text-center">
+          <h1 className="mb-3 text-2xl font-medium tracking-normal leading-relaxed text-foreground/90 text-center">
             Bom dia! Gustavo.
           </h1>
-          <div className="mt-3 p-4 border-2 border-border bg-background">
-            {file ? (
-              <div className="flex gap-4 p-2 border-2 border-border bg-background w-fit max-w-full">
-                {/* <Image height={48} src={pdf} alt="ext file" /> */}
+          <div className="border-2 border-border p-4">
+            {file && (
+              <div
+                className="flex gap-4 p-2 border-2 border-border bg-background w-fit max-w-full 
+               overflow-hidden transition-all duration-300 ease-in-out opacity-100"
+                style={{ maxHeight: "100px" }}
+              >
                 pdf
                 <div className="min-w-0">
-                  <p className="truncate">file_name.pdf</p>
-                  <p className="text-foreground/50">2,323 KB</p>
+                  <p className="truncate">{file.name}</p>
+                  <p className="text-foreground/50">
+                    {(file.size / 1024).toFixed(0)} KB
+                  </p>
                 </div>
               </div>
-            ) : null}
-            <div className="flex">
+            )}
+
+            <div className="flex gap-2">
               <input
                 type="text"
-                className={`w-full ${
-                  file ? "py-4" : "py-2"
-                } focus:outline-none focus:ring-0 focus:border-border`}
+                className="py-4 w-full focus:outline-none focus:ring-0 focus:border-border"
                 placeholder={
                   !file
                     ? "Faça upload do documento para revisão."
@@ -40,18 +55,26 @@ export default function Home() {
                 disabled={!file}
               />
               {file ? (
-                <button className="p-2 flex items-center justify-center cursor-pointer">
+                <button className="w-16 p-2 flex items-center justify-center cursor-pointer">
                   <MoveUp className="text-foreground/90" size={28} />
                 </button>
               ) : (
-                <button className="p-2 flex items-center justify-center cursor-pointer">
-                  <Plus className="text-foreground/90" size={28} />
-                </button>
+                <>
+                  <button
+                    className="w-16 p-2 flex items-center justify-center cursor-pointer"
+                    onClick={handleClick}
+                  >
+                    <Plus className="text-foreground/90" size={28} />
+                  </button>
+                  <input
+                    type="file"
+                    ref={inputRef}
+                    className="hidden"
+                    onChange={handleFileChange}
+                  />
+                </>
               )}
             </div>
-            {/* <div className="text-sm text-gray-500">
-              Seu arquivo está pronto para ser revisado!
-            </div> */}
           </div>
         </div>
       </div>
